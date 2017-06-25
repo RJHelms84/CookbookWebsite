c65631374e20513097405a1d0c04080f14c3dc6e
Robert Helms <robertjohnhelms@gmail.com>
2017-06-25 17:29:36 +0100

    Initial Commit

---
+++ CookBookWebsite/Controllers/AdminController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookBookWebsite.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        CookBookContext db = new CookBookContext();
+
+        [Authorize(Users = "robertjohnhelms@gmail.com")]
+        public ActionResult Index()
+        {
+            return View(db.Recipes.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Recipe newRecipe)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Recipes.Add(newRecipe);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(newRecipe);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Recipe recipe = new Recipe();
+
+            if(ModelState.IsValid)
+            {
+                foreach(Recipe item in db.Recipes)
+                {
+                    if(item.Id == id)
+                    {
+                        recipe.Id = item.Id;
+                        recipe.Name = item.Name;
+                        recipe.ImageUrl = item.ImageUrl;
+                        recipe.CookingTime = item.CookingTime;
+                        recipe.Ingredients = item.Ingredients;
+                        recipe.Directions = item.Directions;
+                    }
+                }
+            }
+
+            return View(recipe);
+        }
+        [HttpPost]
+        public ActionResult Edit(Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (Recipe item in db.Recipes)
+                {
+                    if (item.Id == recipe.Id)
+                    {
+                        item.Name = recipe.Name;
+                        item.ImageUrl = recipe.ImageUrl;
+                        item.CookingTime = recipe.CookingTime;
+                        item.Ingredients = recipe.Ingredients;
+                        item.Directions = recipe.Directions;
+                    }
+                }
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if(ModelState.IsValid)
+            {
+                foreach(Recipe item in db.Recipes)
+                {
+                    if(item.Id == id)
+                    {
+                        db.Recipes.Remove(item);
+                    }
+                }
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CookBookWebsite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookBookWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        CookBookContext db = new CookBookContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Recipes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
